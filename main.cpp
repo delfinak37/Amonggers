ad63b1e83ccb18c6632f56f7b30343a27dc733ba
delfinak37 <delfinak37@mail.ru>
2023-04-25 10:33:28 +0300

    main

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+#include "Triangle.h"
+#include "Ravnostor.h"
+#include "Ravnobed.h"
+#include "Right.h"
+using namespace std;
+
+
+
+int main()
+{
+	
+	
+}
+
